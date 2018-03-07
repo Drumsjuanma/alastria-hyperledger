f8b750222d0d5c3b47be0464dd65c6f110b6b0da
Joan Arcas <drumsjuanma@gmail.com>
2018-03-07 11:48:29 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,14 +3,22 @@ cryptogen generate --config ./crypto-config.yaml
 
 echo -e '\n\n\e[92m//////// --- Creando bloque genesis --- ////////\e[39m'
 export FABRIC_CFG_PATH=$PWD
-configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+configtxgen -profile ThreeOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
 echo -e '\n\n\e[92m/////// --- Creando canal Caixabank-Alastria --- ////////\e[39m'
-export CHANNEL_NAME=mychannel
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+configtxgen -profile AlastriaCaixabankChannel -outputCreateChannelTx ./channel-artifacts/caixabankChannel.tx -channelID caixabankChannel
+
+echo -e '\n\n\e[92m/////// --- Creando canal Silk-Alastria --- ////////\e[39m'
+configtxgen -profile AlastriaSilkChannel -outputCreateChannelTx ./channel-artifacts/silkChannel.tx -channelID silkChannel
 
 echo -e '\n\n\e[92m//////// --- Creando Anchor Peer Caixabank --- ////////\e[39m'
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/CaixabankMSPanchors.tx -channelID $CHANNEL_NAME -asOrg CaixabankMSP
+configtxgen -profile AlastriaCaixabankChannel -outputAnchorPeersUpdate ./channel-artifacts/CaixabankMSPanchors.tx -channelID caixabankChannel -asOrg CaixabankMSP
 
-echo -e '\n\n\e[92m//////// --- Creando Anchor Peer Alastria --- ////////\e[39m'
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/AlastriaMSPanchors.tx -channelID $CHANNEL_NAME -asOrg AlastriaMSP
+echo -e '\n\n\e[92m//////// --- Creando Anchor Peer Silk --- ////////\e[39m'
+configtxgen -profile AlastriaSilkChannel -outputAnchorPeersUpdate ./channel-artifacts/SilkMSPanchors.tx -channelID silkChannel -asOrg SilkMSP
+
+echo -e '\n\n\e[92m//////// --- Creando Anchor Peer Alastria-Caixabank --- ////////\e[39m'
+configtxgen -profile AlastriaCaixabankChannel -outputAnchorPeersUpdate ./channel-artifacts/AlastriaMSPCaixabankMSPanchors.tx -channelID caixabankChannel -asOrg AlastriaMSP
+
+echo -e '\n\n\e[92m//////// --- Creando Anchor Peer Alastria-Silk --- ////////\e[39m'
+configtxgen -profile AlastriaSilkChannel -outputAnchorPeersUpdate ./channel-artifacts/AlastriaMSPSilkMSPanchors.tx -channelID silkChannel -asOrg AlastriaMSP
