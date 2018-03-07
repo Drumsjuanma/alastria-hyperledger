7c864f4b71b96b2c3f610b58d682364791f2a073
Joan Arcas <drumsjuanma@gmail.com>
2018-03-07 17:16:58 +0000

    updated

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo -e '\e[42m//////// --- Iniciando Red AlastriaNet --- ////////\e[49m'
 
 IMAGETAG="latest"
-IMAGE_TAG=$IMAGETAG docker-compose -f docker-compose-cli.yaml up
+IMAGE_TAG=$IMAGETAG docker-compose -f docker-compose-cli.yaml up -d
 
 echo -e '\e[92m//////// --- Exito --- ////////\e[39m'
