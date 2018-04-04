19ca0575a0c3a516983642005904d5cea2c69ae0
Joan Arcas <drumsjuanma@gmail.com>
2018-04-04 10:51:46 +0000

    update org3

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 docker-compose -f docker-compose-cli.yaml down --volumes
 sudo rm -rf channel-artifacts/* && sudo rm -rf crypto-config/*
 sudo rm scripts/*.block
+cd upscale && sudo ./clean.sh
 
---
+++ upscale/cliAccenture.sh
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose-org3.yaml up -d 
+docker exec -it cli-accenture bash
