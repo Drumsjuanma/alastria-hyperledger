6282cc0c6f6d526833f2ff38f706a0a85643569f
Joan Arcas <drumsjuanma@gmail.com>
2018-04-18 17:23:22 +0200

    Update init.sh

--- test/init.sh
+++ test/init.sh
@@ -5,20 +5,39 @@ set -e
 MESSAGE='Usage: init.sg <mode> <node-type> <node-name>
     mode: CURRENT_HOST_IP | auto | backup
     node-type: validator | general
-    node-name: NODE_NAME (example: Alastria)'
+    node-name: NODE_NAME (example: Alastria)' 
     
-    
-    
-NAME="$1"
 
+if ( [ $# -ne 3 ] ); then
+    echo "$MESSAGE"
+    exit
+fi    
+    
+DOMAIN="$1"
+NAME=${DOMAIN^}
 
 
 echo '
 PeerOrgs:
   - Name: '$NAME'
-    Domain: '$NAME'.alastria.com
+    Domain: '$DOMAIN'.alastria.com
     Template:
       Count: 1
     Users:
       Count: 1
  ' >> crypto-config.yaml
+ 
+ 
+ echo '
+Organizations:
+    - &'$NAME'
+        Name: '$NAME'MSP
+        ID: '$NAME'MSP
+        MSPDir: crypto-config/peerOrganizations/'$DOMAIN'.alastria.com/msp
+
+        AnchorPeers:
+            - Host: peer0.'$DOMAIN'.alastria.com
+              Port: 7051
+' >> configtx.yaml
+
+
