f5f07f8549472e4757d7bc50a542a55bfd63bdbc
Joan Arcas <drumsjuanma@gmail.com>
2018-04-18 18:22:56 +0200

    Update init.sh

--- test/init.sh
+++ test/init.sh
@@ -16,19 +16,36 @@ fi
 DOMAIN="$1"
 NAME=${DOMAIN^}
 
-
-echo '
+if ( [ "$NAME" = "Alastria" ] ); then
+    echo "Nodo Alastria"
+    
+    echo '
+################################################################################
+# Orderers
+################################################################################
+OrdererOrgs:
+  - Name: Orderer
+    Domain: alastria.com
+    Specs:
+      - Hostname: orderer
+      
+################################################################################
+# Peers
+################################################################################      
 PeerOrgs:
   - Name: '$NAME'
     Domain: '$DOMAIN'.alastria.com
     Template:
       Count: 1
     Users:
-      Count: 1
- ' > crypto-config.yaml
- 
- 
- echo '
+      Count: 1' > crypto-config.yaml
+      
+      
+   echo '
+
+################################################################################
+# organizations
+################################################################################
 Organizations:
     - &'$NAME'
         Name: '$NAME'MSP
@@ -38,18 +55,59 @@ Organizations:
         AnchorPeers:
             - Host: peer0.'$DOMAIN'.alastria.com
               Port: 7051
+################################################################################
+#   SECTION: Orderer
+################################################################################
+Orderer: &OrdererDefaults
+    OrdererType: solo
+    Addresses:
+        - orderer.alastria.com:7050
+    BatchTimeout: 2s
+    BatchSize:
+        MaxMessageCount: 10
+        AbsoluteMaxBytes: 99 MB
+        PreferredMaxBytes: 512 KB
+
+    Kafka:
+        Brokers:
+            - 127.0.0.1:9092
+
+    Organizations:
 ' > configtx.yaml
-
-
-
-if ( [ "$NAME" = "Alastria" ] ); then
-    echo "Nodo Alastria"
+      
+      
+      
+    
+else
     echo '
-OrdererOrgs:
-  - Name: Orderer
-    Domain: alastria.com
-    Specs:
-      - Hostname: orderer' >> crypto-config.yaml
-    exit
+    ################################################################################
+    # Peers
+    ################################################################################
+    PeerOrgs:
+      - Name: '$NAME'
+        Domain: '$DOMAIN'.alastria.com
+        Template:
+          Count: 1
+        Users:
+          Count: 1
+     ' > crypto-config.yaml
+
+
+     echo '
+    ################################################################################
+    # organizations
+    ################################################################################
+    Organizations:
+        - &'$NAME'
+            Name: '$NAME'MSP
+            ID: '$NAME'MSP
+            MSPDir: crypto-config/peerOrganizations/'$DOMAIN'.alastria.com/msp
+
+            AnchorPeers:
+                - Host: peer0.'$DOMAIN'.alastria.com
+                  Port: 7051
+    ' > configtx.yaml
+    
 fi
+
 
